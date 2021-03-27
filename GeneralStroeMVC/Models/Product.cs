cb9e97e10ebe3c374f3e7b7b0fcf3eb5527a5e3f
dawita <dawit.fitru@gmail.com>
2021-03-27 10:31:15 -0400

    first Commit
    first commit for Product

---
+++ GeneralStroeMVC/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStroeMVC.Models
+{
+    public class Product
+    {[Key]
+        public int ProductID    { get; set; }//Key Field
+        [Required]
+        [Display(Name="Product Name")]
+        public string Name  { get; set; }
+        public int InventoryCount { get; set; }
+        [Required]
+        public decimal  Price { get; set; }
+        [Required]
+        public bool IsFood  { get; set; }
+    }
+    public class GeneralStoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
